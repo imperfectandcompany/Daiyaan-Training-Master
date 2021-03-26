d15759aaba228ceecbd3f6aea930e17450ebef8e
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-03-26 16:34:36 -0400

    Notes on form atoms

--- index.php
+++ index.php
@@ -11,5 +11,19 @@
 <body>
 <h1>Login</h1>
 <p>Login with a username and pass</p>
+
+
+<!-- allows us to define what type of method the information is going to be processed through using the method attribute, for example... POST method vs. GET method.
+ex. method="post" vs method="get"
+It also allows us to define where the information is being sent, it could be sent to the same page or another page using the 
+attribute action
+ex. action=</?php echo _SERVER['PHP_SELF']?/> << this sends the action to the same page
+vs action="process.php"
+-->
+<form>
+<input></input>
+<!-- allows us to define if this button pertains to a submission using the attribute type -->
+<button></button>
+</form>
 </body>
 </html>
