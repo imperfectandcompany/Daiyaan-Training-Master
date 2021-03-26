2fb9ad046402003989cc4228e251386141a6686a
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2021-03-26 16:20:37 -0400

    boilerplate update

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
+<!doctype html>
 <html>
-
 <head>
-<title>Login - Postogon</title>
-<link rel="" href=""></link>
+  <meta charset="UTF-8" />
+  <title>Login - Postogon</title>  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+  <!-- ... -->
 </head>
-
-
 <body>
-<h1>header</h1>
-<p>test</p>
+<h1>Login</h1>
+<p>Login with a username and pass</p>
 </body>
-
 </html>
